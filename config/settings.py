719810af0f72ce0b925be79e1f5a33bea382c17a
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-02-02 01:44:34 +0300

    add: tempalte

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates' # Path: config/settings.py
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
