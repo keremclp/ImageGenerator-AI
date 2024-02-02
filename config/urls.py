0e1845e49a1b5b87f0dfe344b8c5e9532146999e
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-02-02 03:07:57 +0300

    fix: photo media issue

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 from config.views import main_home
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_home, name='home'),
     path('', include('images.urls', namespace='images')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
